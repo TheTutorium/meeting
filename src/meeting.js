25af8eafc6393d2a013d9c3244b3cd26cf4cf29b
ozc0 <oguzhanozcelik0151@gmail.com>
2023-05-17 21:52:56 +0300

    oyleboyle

--- src/meeting.js
+++ src/meeting.js
@@ -22,6 +22,7 @@ let renderVideo = (stream) => {
 // Register with the peer server
 export let peer = new Peer({
     host: "/",
+    port: 3000,
     path: "/peerjs/myapp",
 });
 
@@ -34,141 +35,6 @@ peer.on("open", (id) => {
 });
 peer.on("error", (error) => {
     console.error(error);
-});
-
-// Handle incoming data connection
-peer.on("connection", (conn) => {
-    otherPeer = conn;
-    conn.on("data", (data) => {
-        if (!connectionInitiated) {
-            connectionInitiated = true;
-        } else {
-
-            console.log(data);
-            let splittedMessage = data.split("|");
-
-            let tempPenType = parseInt(splittedMessage[0]);
-            setCurrentZIndex(parseInt(splittedMessage[1]));
-
-            if(tempPenType >= 0){
-                setHistory(history + data + "\n");
-            }
-
-            if(tempPenType == 0){ // Pen
-                let initX = parseFloat(splittedMessage[2]);
-                let initY = parseFloat(splittedMessage[3]);
-                let control1x = parseFloat(splittedMessage[4]);
-                let control1y = parseFloat(splittedMessage[5]);
-                let control2x = parseFloat(splittedMessage[6]);
-                let control2y = parseFloat(splittedMessage[7]);
-                let finalX = parseFloat(splittedMessage[8]);
-                let finalY = parseFloat(splittedMessage[9]);
-                let tempPenSize = parseInt(splittedMessage[10]);
-                let tempPenColor = parseInt(splittedMessage[11]);
-
-                let tempSprite = new PIXI.Graphics();
-
-                tempSprite.lineStyle(tempPenSize, tempPenColor, 1);
-
-                tempSprite.zIndex = currentZIndex;
-                tempSprite.moveTo(initX, initY);
-                tempSprite.bezierCurveTo(control1x, control1y, control2x, control2y, finalX, finalY);
-    
-                stage.addChild(tempSprite);
-            }else if(tempPenType == 1){ // Eraser
-                let initX = parseFloat(splittedMessage[2]);
-                let initY = parseFloat(splittedMessage[3]);
-                let control1x = parseFloat(splittedMessage[4]);
-                let control1y = parseFloat(splittedMessage[5]);
-                let control2x = parseFloat(splittedMessage[6]);
-                let control2y = parseFloat(splittedMessage[7]);
-                let finalX = parseFloat(splittedMessage[8]);
-                let finalY = parseFloat(splittedMessage[9]);
-                let tempEraserSize = parseInt(splittedMessage[10]);
-
-                let tempSprite = new PIXI.Graphics();
-
-                tempSprite.lineStyle(tempEraserSize, 0xffffff, 1);
-
-                tempSprite.zIndex = currentZIndex;
-                tempSprite.moveTo(initX, initY);
-                tempSprite.bezierCurveTo(control1x, control1y, control2x, control2y, finalX, finalY);
-    
-                stage.addChild(tempSprite);
-            }else if(tempPenType == 2){ // Typing
-                let tempTextSize = parseInt(splittedMessage[2]);
-                let tempTextColor = parseInt(splittedMessage[3]);
-                let tempText = splittedMessage[4];
-                let tempX = parseFloat(splittedMessage[5]);
-                let tempY = parseFloat(splittedMessage[6]);
-                
-                let tempStyle = new PIXI.TextStyle({
-                    fontFamily: "Arial",
-                    fontSize: tempTextSize,
-                    fill: tempTextColor,
-                });
-
-                let tempPText = new PIXI.Text(tempText, tempStyle);
-                tempPText.zIndex = currentZIndex;
-                tempPText.x = tempX;
-                tempPText.y = tempY;
-
-                stage.addChild(tempPText);
-            }else if(tempPenType == 3){
-                let temp_image = splittedMessage[2];
-                let tempX = parseFloat(splittedMessage[3]);
-                let tempY = parseFloat(splittedMessage[4]);
-
-                const image_texture = PIXI.Texture.from(temp_image);
-                const sprite = new PIXI.Sprite(image_texture);
-
-                sprite.x = tempX;
-                sprite.y = tempY;
-
-                stage.addChild(sprite);
-            }else if(tempPenType == -1){
-                let temp_pdf = splittedMessage[1];
-
-                const arr = temp_pdf.split(",").map(Number);
-                const uint8arr = new Uint8Array(arr);
-
-                console.log(uint8arr);
-                
-
-                pdfjsLib.getDocument(uint8arr).promise.then(function(pdf) {
-                    var pages = Array.from(Array(pdf.numPages).keys());
-                    return Promise.all(pages.map(function(num) {
-                    return pdf.getPage(num + 1);
-                    }));
-                }).then(function(pages) {
-                    
-                    var iframe = document.getElementById('pdf-iframe');
-                    canvasElements = pages.map(function(page) {
-                        var canvas = document.createElement('canvas');
-                        console.log(iframe);
-                        var scale = Math.min(iframe.clientWidth / page.getViewport({scale: 1}).width, iframe.clientHeight / page.getViewport({scale: 1}).height);
-                        var viewport = page.getViewport({scale: scale * 1.3});
-                        canvas.width = viewport.width;
-                        canvas.height = viewport.height;
-                        page.render({canvasContext: canvas.getContext('2d'), viewport: viewport}).promise.then(function() {});
-                        return canvas;
-                    });
-                    
-                    var doc = iframe.contentWindow.document;
-                    doc.open();
-                    doc.write("<html><body></body></html>");
-                    canvasElements.forEach(function(canvas) {
-                    doc.body.appendChild(canvas);
-                    });
-                    doc.close();
-                });
-            }
-
-        }
-    });
-    conn.on("open", () => {
-        conn.send("hello!");
-    });
 });
 
 // Handle incoming voice/video connection
@@ -365,29 +231,145 @@ let disconnectFromPeer = () => {
         screenStream = null;
     }
 };
-
-let shareScreen = () => {
-    navigator.mediaDevices
-      .getDisplayMedia({ video: true })
-      .then((screenStream) => {
-        const videoTrack = screenStream.getVideoTracks()[0];
-        const sender = currentCall.peerConnection.getSenders().find((s) => s.track.kind === videoTrack.kind);
-        sender.replaceTrack(videoTrack);
   
-        // Create a new video element for the shared screen
-        const sharedScreenVideo = document.createElement("video");
-        sharedScreenVideo.srcObject = screenStream;
-        sharedScreenVideo.autoplay = true;
-        sharedScreenVideo.classList.add("shared-screen");
-  
-        // Append the shared screen video element to the HTML body
-        document.body.appendChild(sharedScreenVideo);
-      })
-      .catch((error) => {
-        console.error("Error sharing screen:", error);
-      });
-  };
-  
-
 window.connectToPeer = connectToPeer;
 window.disconnectFromPeer = disconnectFromPeer;
+
+
+
+
+/// whiteboard related
+// Handle incoming data connection
+peer.on("connection", (conn) => {
+    otherPeer = conn;
+    conn.on("data", (data) => {
+        if (!connectionInitiated) {
+            connectionInitiated = true;
+        } else {
+
+            console.log(data);
+            let splittedMessage = data.split("|");
+
+            let tempPenType = parseInt(splittedMessage[0]);
+            setCurrentZIndex(parseInt(splittedMessage[1]));
+
+            if(tempPenType >= 0){
+                setHistory(history + data + "\n");
+            }
+
+            if(tempPenType == 0){ // Pen
+                let initX = parseFloat(splittedMessage[2]);
+                let initY = parseFloat(splittedMessage[3]);
+                let control1x = parseFloat(splittedMessage[4]);
+                let control1y = parseFloat(splittedMessage[5]);
+                let control2x = parseFloat(splittedMessage[6]);
+                let control2y = parseFloat(splittedMessage[7]);
+                let finalX = parseFloat(splittedMessage[8]);
+                let finalY = parseFloat(splittedMessage[9]);
+                let tempPenSize = parseInt(splittedMessage[10]);
+                let tempPenColor = parseInt(splittedMessage[11]);
+
+                let tempSprite = new PIXI.Graphics();
+
+                tempSprite.lineStyle(tempPenSize, tempPenColor, 1);
+
+                tempSprite.zIndex = currentZIndex;
+                tempSprite.moveTo(initX, initY);
+                tempSprite.bezierCurveTo(control1x, control1y, control2x, control2y, finalX, finalY);
+    
+                stage.addChild(tempSprite);
+            }else if(tempPenType == 1){ // Eraser
+                let initX = parseFloat(splittedMessage[2]);
+                let initY = parseFloat(splittedMessage[3]);
+                let control1x = parseFloat(splittedMessage[4]);
+                let control1y = parseFloat(splittedMessage[5]);
+                let control2x = parseFloat(splittedMessage[6]);
+                let control2y = parseFloat(splittedMessage[7]);
+                let finalX = parseFloat(splittedMessage[8]);
+                let finalY = parseFloat(splittedMessage[9]);
+                let tempEraserSize = parseInt(splittedMessage[10]);
+
+                let tempSprite = new PIXI.Graphics();
+
+                tempSprite.lineStyle(tempEraserSize, 0xffffff, 1);
+
+                tempSprite.zIndex = currentZIndex;
+                tempSprite.moveTo(initX, initY);
+                tempSprite.bezierCurveTo(control1x, control1y, control2x, control2y, finalX, finalY);
+    
+                stage.addChild(tempSprite);
+            }else if(tempPenType == 2){ // Typing
+                let tempTextSize = parseInt(splittedMessage[2]);
+                let tempTextColor = parseInt(splittedMessage[3]);
+                let tempText = splittedMessage[4];
+                let tempX = parseFloat(splittedMessage[5]);
+                let tempY = parseFloat(splittedMessage[6]);
+                
+                let tempStyle = new PIXI.TextStyle({
+                    fontFamily: "Arial",
+                    fontSize: tempTextSize,
+                    fill: tempTextColor,
+                });
+
+                let tempPText = new PIXI.Text(tempText, tempStyle);
+                tempPText.zIndex = currentZIndex;
+                tempPText.x = tempX;
+                tempPText.y = tempY;
+
+                stage.addChild(tempPText);
+            }else if(tempPenType == 3){
+                let temp_image = splittedMessage[2];
+                let tempX = parseFloat(splittedMessage[3]);
+                let tempY = parseFloat(splittedMessage[4]);
+
+                const image_texture = PIXI.Texture.from(temp_image);
+                const sprite = new PIXI.Sprite(image_texture);
+
+                sprite.x = tempX;
+                sprite.y = tempY;
+
+                stage.addChild(sprite);
+            }else if(tempPenType == -1){
+                let temp_pdf = splittedMessage[1];
+
+                const arr = temp_pdf.split(",").map(Number);
+                const uint8arr = new Uint8Array(arr);
+
+                console.log(uint8arr);
+                
+
+                pdfjsLib.getDocument(uint8arr).promise.then(function(pdf) {
+                    var pages = Array.from(Array(pdf.numPages).keys());
+                    return Promise.all(pages.map(function(num) {
+                    return pdf.getPage(num + 1);
+                    }));
+                }).then(function(pages) {
+                    
+                    var iframe = document.getElementById('pdf-iframe');
+                    canvasElements = pages.map(function(page) {
+                        var canvas = document.createElement('canvas');
+                        console.log(iframe);
+                        var scale = Math.min(iframe.clientWidth / page.getViewport({scale: 1}).width, iframe.clientHeight / page.getViewport({scale: 1}).height);
+                        var viewport = page.getViewport({scale: scale * 1.3});
+                        canvas.width = viewport.width;
+                        canvas.height = viewport.height;
+                        page.render({canvasContext: canvas.getContext('2d'), viewport: viewport}).promise.then(function() {});
+                        return canvas;
+                    });
+                    
+                    var doc = iframe.contentWindow.document;
+                    doc.open();
+                    doc.write("<html><body></body></html>");
+                    canvasElements.forEach(function(canvas) {
+                    doc.body.appendChild(canvas);
+                    });
+                    doc.close();
+                });
+            }
+
+        }
+    });
+    conn.on("open", () => {
+        conn.send("hello!");
+    });
+});
