f8e1d610a5103a19667a9f1ec83fe151849e7466
ozc0 <oguzhanozcelik0151@gmail.com>
2023-05-17 22:35:31 +0300

    Assigned Peer Id logic by path added

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ app.get("/", (request, response) => {
     response.sendFile(__dirname + "/public/index.html");
 });
 
+// assigned id version
+app.get("/:id1/:id2", (request, response) => {
+    const id1 = request.params.id1;
+    const id2 = request.params.id2;
+  
+    // Perform your desired operations or logic with id1 and id2
+    // For example, you can establish a peer-to-peer connection using these IDs
+    response.sendFile(__dirname + "/public/index.html");
+  
+    // Send a response back to the client
+    //res.send(`Connected to peer ${id2} using peer ID ${id1}`);
+  });
+
 // Set headers for all routes
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
